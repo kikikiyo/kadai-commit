11bc800ba2f505cb2fd2e94fd188cdab05e45178
kiki.kiyo <kiyo0619japan@gmail.com>
2020-08-17 07:31:56 +0000

    add omikuji function

--- sample.php
+++ sample.php
@@ -1,5 +1,13 @@
 <?php
     $result = "無し";
+    if (array_key_sxists('act',$_POST)) {
+        $result = omikuji();
+    }
+    
+    function omikuji() {
+        $fortun = ["大吉","吉","中吉","小吉","末吉","凶"];
+        return $fortune[random_int(0, count($fortune) -1)];
+    }
 ?>
 <!DOCTYPE html>
 <html lang="ja">
